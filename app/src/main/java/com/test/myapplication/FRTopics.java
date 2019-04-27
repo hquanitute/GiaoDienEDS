cfcbff93aca33db2ce9e7f533d46d6039f579e1b
hquanitute <hquanitute@gmail.com>
2019-04-27 18:22:49 +0700

    Nghe duoc nguoi noi

--- app/src/main/java/com/test/myapplication/FRTopics.java
+++ app/src/main/java/com/test/myapplication/FRTopics.java
@@ -1,12 +1,15 @@
 package com.test.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.test.myapplication.Adapter.TopicAdapter;
 import com.test.myapplication.objects.Topic;
@@ -19,19 +22,27 @@ public class FRTopics extends Fragment {
     ListView lvTopics;
     ArrayList<Topic> topics;
     TopicAdapter adapter;
+    View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_topics, container, false);
+        view =inflater.inflate(R.layout.fragment_topics, container, false);
         lvTopics = view.findViewById(R.id.listviewTopics);
+        lvTopics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                doChallengen(topics.get(position));
+            }
+        });
         topics = new ArrayList<>();
         topics.add(new Topic("Bài 1", "Hello everybody"));
-        topics.add(new Topic("Bài 2","My name is Hoang Quan adad adad adad adad adad dad adadad adada dada dada "));
+        topics.add(new Topic("Bài 2","In the past, many such disputes have been handled through negotiation. In 1796, George Washington refused a House request for documents concerning the Jay Treaty with Britain, arguing that treaties were the sole purview of the Senate — to which he did release the papers. "));
         topics.add(new Topic("Bài 2","My name is Hoang Quan"));
         topics.add(new Topic("Bài 2","My name is Hoang Quan"));
         topics.add(new Topic("Bài 2","My name is Hoang Quan"));
@@ -39,5 +50,10 @@ public class FRTopics extends Fragment {
         lvTopics.setAdapter(adapter);
         return view;
     }
-
+    public void doChallengen(Topic topic){
+        Intent intent = new Intent(getActivity(),Challengen.class);
+        intent.putExtra("topic_name",topic.getName());
+        intent.putExtra("topic_text",topic.getText());
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,6 +1,13 @@
 package com.test.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermission();
         tabLayout= findViewById(R.id.tabs);
         viewPager= findViewById(R.id.vp_main);
         ViewPaperAdapter viewPaperAdapter = new ViewPaperAdapter(getSupportFragmentManager());
@@ -25,5 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(viewPaperAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
