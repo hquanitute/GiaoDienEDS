1f38161abc717c58a9a7f910c1928b05d424bd98
hquanitute <hquanitute@gmail.com>
2019-04-27 12:22:32 +0700

    Giao dien tablayout

---
+++ app/src/main/java/com/test/myapplication/FRTCustom.java
@@ -0,0 +1,34 @@
+package com.test.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.test.myapplication.Adapter.TopicAdapter;
+import com.test.myapplication.objects.Topic;
+
+import java.util.ArrayList;
+
+public class FRTCustom extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_topics,container,false);
+        ListView listView = view.findViewById(R.id.listviewTopics);
+        ArrayList<Topic> topics = new ArrayList<>();
+        topics.add(new Topic("Custom1","Customadadadadad"));
+        TopicAdapter topicAdapter = new TopicAdapter(view.getContext(),R.layout.topic_row,topics);
+        listView.setAdapter(topicAdapter);
+        return view;
+    }
+}
--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.test.myapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.test.myapplication.Adapter.ViewPaperAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout= findViewById(R.id.tabs);
+        viewPager= findViewById(R.id.vp_main);
+        ViewPaperAdapter viewPaperAdapter = new ViewPaperAdapter(getSupportFragmentManager());
+        viewPaperAdapter.AddFragment(new FRTopics(),"Topics");
+        viewPaperAdapter.AddFragment(new FRTCustom(),"Custom");
+        viewPaperAdapter.AddFragment(new user(),"User");
+
+        viewPager.setAdapter(viewPaperAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
