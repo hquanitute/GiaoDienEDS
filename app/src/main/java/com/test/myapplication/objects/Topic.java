0e4f4445afd230154ecc29d603ac45e6fb16c3a4
hquanitute <hquanitute@gmail.com>
2019-04-28 22:43:12 +0700

    add Objects

--- app/src/main/java/com/test/myapplication/objects/Topic.java
+++ app/src/main/java/com/test/myapplication/objects/Topic.java
@@ -1,13 +1,19 @@
 package com.test.myapplication.objects;
 
 public class Topic {
+    int id;
+    int diem;
+    int loai;
     String name;
     String text;
 
     public Topic() {
     }
 
-    public Topic(String name, String text) {
+    public Topic(int id, int diem, int loai, String name, String text) {
+        this.id = id;
+        this.diem = diem;
+        this.loai = loai;
         this.name = name;
         this.text = text;
     }
@@ -26,5 +32,29 @@ public class Topic {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDiem() {
+        return diem;
+    }
+
+    public void setDiem(int diem) {
+        this.diem = diem;
+    }
+
+    public int getLoai() {
+        return loai;
+    }
+
+    public void setLoai(int loai) {
+        this.loai = loai;
     }
 }
